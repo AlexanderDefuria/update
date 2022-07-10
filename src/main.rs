185d2c7ceef2b82bc1c3cb7a90ba0af4db0ba13d
Alexander De Furia <defuria.alexander@outlook.com>
2022-07-10 01:28:02 -0400

    dotenv

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use httparse::Header;
 use serde_json::Value;
 use sha2::Sha256;
 use hex::encode;
+use dotenv;
+use std::io;
 
 type HmacSha256 = Hmac<Sha256>;
 
@@ -25,7 +27,7 @@ fn handle_conn(mut stream: TcpStream) {
 
     let mut headers = [httparse::EMPTY_HEADER; 16];
     let mut req = httparse::Request::new(&mut headers);
-    req.parse(&buffer).unwrap();
+    let header_size = req.parse(&buffer).unwrap().unwrap();
 
     if !headers.is_empty() {
         for header in headers {
@@ -37,22 +39,34 @@ fn handle_conn(mut stream: TcpStream) {
                     println!("{} : {}", header.name, String::from_utf8_lossy(header.value));
                 }
                 Header { name: "X-Hub-Signature-256", .. } => {
-                    println!("{} : {}", header.name, String::from_utf8_lossy(header.value));
+                    //println!("{} : {}", header.name, String::from_utf8_lossy(header.value));
                     let mut start_of_body: usize = 0;
                     while start_of_body < buffer.len() {
                         // TODO Make more robust check using carriage returns and line breaks.
                         if buffer[start_of_body] == '{' as u8 { break;} else { start_of_body += 1;}
                     }
                     let body = &buffer[start_of_body..actual_size];
-                    let body_json: Value = serde_json::from_slice(&body).expect("Cannot Parse JSON");
-                    println!("Body: {} \n", String::from_utf8_lossy(&body));
+                    //println!("Body: {} \n", String::from_utf8_lossy(&body));
 
                     let key = "password";
                     let mut mac = HmacSha256::new_from_slice(key.as_bytes()).expect("");
                     mac.update(body);
 
                     if String::from_utf8_lossy(&header.value[7..]) != encode(mac.finalize().into_bytes()) {
-                        return
+                        //return
+                        // TODO Re-enable for actual webhooks
+                    }
+
+                    let body_json: Value = serde_json::from_slice(&body).expect("Cannot Parse JSON");
+
+                    match dotenv::var("ACTION").unwrap().as_str() {
+                        "DOCKER" => {
+                            println!("Docker")
+                        },
+                        "GITHUB" => {
+                            println!("Github")
+                        }
+                        _ => {}
                     }
                 }
                 _ => {}
